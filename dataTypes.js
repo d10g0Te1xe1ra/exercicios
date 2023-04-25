dab6c549f1dc8815669fce75f85a1c9c748d33be
d10g0Te1xe1ra <diogoteixeira455@gmail.com>
2023-04-25 19:35:08 +0100

    exercicios tipos de dados

---
+++ dataTypes.js
@@ -0,0 +1,15 @@
+const daysOffJs = '30 Days Of JavaScript'
+console.log(daysOffJs.length)
+console.log(daysOffJs.toUpperCase())
+console.log(daysOffJs.slice(11, 21))
+console.log(daysOffJs.includes('JavaScript'))
+const empresas = 'Facebook,Google,Microsoft,Apple,IBM,Oracle,Amazon'
+console.log(empresas.split(','));
+console.log(typeof(empresas.split(',')));
+console.log(empresas.split(',').length);
+console.log(daysOffJs.replace('JavaScript', 'Coding'))
+console.log (Math.floor(Math.random()* 100)+ 1)
+const js = 'JavaScript'
+let index = Math.floor(Math.random()* 10)
+console.log(js.charAt(index))
+console.log(js[index])
