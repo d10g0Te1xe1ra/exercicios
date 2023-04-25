c7b2aa2daab45e677b3bc7b2f226856ad3b43c74
d10g0Te1xe1ra <diogoteixeira455@gmail.com>
2023-04-25 18:47:36 +0100

    exercicios comentarios e variaveis

---
+++ playground.js
@@ -0,0 +1,16 @@
+let escrita = 'Bom dia' ;
+const shitch = true ;
+var indefenido = undefined ;
+const vazio = null ;
+const number = 7 ;
+//Os comentários tornam o código legível
+let ground ;
+let dear;
+var hoje
+let small
+const primeiroNome = 'Diogo' ;
+const ultimoNome = 'Teixeira' ;
+const idade = 15
+const nacionalidade = 'Portuguesa';
+const animePreferido = 'My Star'
+console.log(`O meu nome é ${primeiroNome} ${ultimoNome}. Eu tenho ${idade} anos de idade. Tenho nacionalidade ${nacionalidade}. E o meu anime preferido é ${animePreferido}. `)
