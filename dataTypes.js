29c88e3a9090de45de7bba953c45137415640392
d10g0Te1xe1ra <diogoteixeira455@gmail.com>
2023-04-27 18:55:03 +0100

    exercicios de data types

--- dataTypes.js
+++ dataTypes.js
@@ -13,3 +13,18 @@ const js = 'JavaScript'
 let index = Math.floor(Math.random()* 10)
 console.log(js.charAt(index))
 console.log(js[index])
+
+const frase = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+let parts = frase.split(', ')
+console.log(parts[0])
+console.log(parts[0].slice(9, 13))
+const salary = eval(parts[0].slice(9, 13))
+console.log(parts[1])
+console.log(parts[1].slice(0, 5))
+const bonus = eval(parts[1].slice(0, 5))
+console.log(parts[2])
+console.log(parts[2].slice(0, 5))
+const courses = eval(parts[2].slice(0, 5))
+const total = salary + bonus + courses 
+console.log(total)
